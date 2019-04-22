d32b062e0f7a62e17480cbff335c58f61924baa4
Sathish Kumar Reddy Bijjam <32219171+sathish277@users.noreply.github.com>
2019-04-22 11:03:07 +0530

    Update data_preprocessing_custome_company.py

--- data_preprocessing_custome_company.py
+++ data_preprocessing_custome_company.py
@@ -1,58 +1,18 @@
-
+#in this only we handle the missingvalues
 # coding: utf-8
-
-# In[1]:
-
-
+#import neccessary libraries
+import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
-
-# In[60]:
-
+#read the csv data file using pandas
 
 data=pd.read_csv("C:/data preprocessing/customers_company/data.csv")
 
+#inspection of data
+print(data.head())
 
-# In[61]:
-
-
-data
-
-
-# In[74]:
-
-
-for i in data.columns:
-    print(i+":",end=" ")
-    print(data[i].dtype)    
-#data.dtypes
-
-
-# In[63]:
-
-
-for i in data.columns:
-    data_type=data[i].dtype
-    if(sum(data[i].isnull())>0):
-        if(data_type!='object'):
-            data[i].fillna(data[i].median(),inplace=True)  
-
-
-# In[64]:
-
-
-data
-
-
-# In[65]:
-
-
-import matplotlib.pyplot as plt
-
-
-# In[75]:
-
-
+#graphical representation of replation between each feature and label
 x=data['Purchased']
 for i in range(len(data.columns)-1):
     plt.figure(i+1)
@@ -61,4 +21,27 @@ for i in range(len(data.columns)-1):
     plt.xlabel('Purchased')
     plt.grid()
     plt.show()
+
+#data types of features in the data 
+for i in data.columns:
+    print(i+":",end=" ")
+    print(data[i].dtype)
+#alternate way to knowing the datatypes of features
+#data.dtypes
+
+#for knowing which features have missing values
+for i in data.columns:
+    print(sum(data[i].isnull()))
+
+#handling missing values
+for i in data.columns:
+    data_type=data[i].dtype
+    if(sum(data[i].isnull())>0):
+        data[i].fillna(method='ffill',inplace=True)  
+
+#after handling missing values
+print(data)
+
+
+
 
